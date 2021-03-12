29ae5d936538d67ead315edc7997a2f959131804
MrP01 <peter@waldert.at>
2021-03-12 11:34:13 +0100

    store and load training data from numpy into classifier as xtensor

--- code/classifier/main.cpp
+++ code/classifier/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <xtensor/xnpy.hpp>
+#include <xtensor/xio.hpp>
 #include "Network.h"
 
 int main() {
@@ -7,5 +9,12 @@ int main() {
   neuralNet->addLayer(784, 20);
   neuralNet->addLayer(20, 20);
   neuralNet->addLayer(20, 10);
+
+  auto x_train = xt::load_npy<float>("data/mnist/x-train.npy");
+  auto y_train = xt::load_npy<int>("data/mnist/y-train.npy");
+  auto x_test = xt::load_npy<float>("data/mnist/x-test.npy");
+  auto y_test = xt::load_npy<int>("data/mnist/y-test.npy");
+  std::cout << x_test << std::endl;
+  std::cout << y_test << std::endl;
   return 0;
 }
--- code/classifier/tasks.py
+++ code/classifier/tasks.py
@@ -1,9 +1,16 @@
+import os
+
 import invoke
 import mnist
+import numpy as np
 
 
 @invoke.task()
-def fetch_training_data(ctx):
+def fetch_training_data(ctx, target="data/mnist"):
     """Download MNIST training data"""
-    data = mnist.mnist()
-    print(data)
+    x_train, y_train, x_test, y_test = mnist.mnist()
+    ctx.run(f"mkdir -p {target}")
+    np.save(os.path.join(target, "x-train.npy"), x_train)
+    np.save(os.path.join(target, "y-train.npy"), y_train)
+    np.save(os.path.join(target, "x-test.npy"), x_test)
+    np.save(os.path.join(target, "y-test.npy"), y_test)
