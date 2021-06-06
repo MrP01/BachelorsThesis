9563e7f6d9f97e899f0cc7fa10942453e6d02839
MrP01 <peter@waldert.at>
2021-06-06 03:00:59 +0200

    use react-painter for a drawing frontend

--- code/frontend/src/App.js
+++ code/frontend/src/App.js
@@ -1,14 +1,19 @@
 import 'materialize-css';
 import 'materialize-css/dist/css/materialize.css';
-import {Button, Col, Icon, Navbar, Row} from 'react-materialize';
+import { Button, Col, Icon, Navbar, Row } from 'react-materialize';
+import { ReactPainter } from 'react-painter';
 import "./App.css";
+
+function classify(blob) {
+  console.log("Classifying given input", blob);
+}
 
 function App() {
   return (
     <div>
       <Navbar
         alignLinks="right"
-        brand={<span style={{"padding-left": "10px"}}>FHE Classifier</span>}
+        brand={<span style={{ "paddingLeft": "10px" }}>FHE Classifier</span>}
         id="mobile-nav"
         menuIcon={<Icon>menu</Icon>}
       >
@@ -17,7 +22,21 @@ function App() {
         <Col s={12}>
           <h3 className={"center"}>Classify your Secret Data</h3>
           <p>Using Fully Homomorphic Encryption, directly from within the browser.</p>
-          <Button>Upload</Button>
+          <ReactPainter
+            width={300}
+            height={300}
+            initialColor={"cornflowerblue"}
+            initialLineWidth={20}
+            initialLineJoin={"miter"}
+            lineCap={"round"}
+            onSave={classify}
+            render={({ triggerSave, canvas, setColor }) => (
+              <div>
+                <div className={"canvas-container center"}>{canvas}</div>
+                <Button onClick={triggerSave}>Classify</Button>
+              </div>
+            )}
+          />
         </Col>
       </Row>
     </div>
