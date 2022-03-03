752a02a5d2fdf331d06005ad11e58968e58225a4
MrP01 <peter@waldert.at>
2022-03-03 16:09:35 +0100

    clang-format

--- code/classifier/Layer.h
+++ code/classifier/Layer.h
@@ -1,9 +1,9 @@
 #pragma once
 
-#include <xtensor/xarray.hpp>
 #include "seal/ciphertext.h"
-#include "seal/evaluator.h"
 #include "seal/ckks.h"
+#include "seal/evaluator.h"
+#include <xtensor/xarray.hpp>
 
 typedef xt::xarray<double> Matrix;
 typedef xt::xarray<double> Vector;
@@ -11,42 +11,47 @@ typedef xt::xarray<double> Vector;
 class Network;
 
 class Layer {
-  friend Network;  // Network can access my properties
+  friend Network; // Network can access my properties
 
-  protected:
-    Network* parent = nullptr;
+ protected:
+  Network *parent = nullptr;
 
-  public:
-    Layer() = default;
-    virtual Vector feedforward(Vector x) = 0;
-    virtual void feedforwardEncrypted(seal::Ciphertext &in_out, seal::GaloisKeys &galoisKeys, seal::RelinKeys relinKeys, seal::CKKSEncoder &ckksEncoder, seal::Evaluator &evaluator) = 0;
+ public:
+  Layer() = default;
+  virtual Vector feedforward(Vector x) = 0;
+  virtual void feedforwardEncrypted(seal::Ciphertext &in_out, seal::GaloisKeys &galoisKeys, seal::RelinKeys relinKeys, seal::CKKSEncoder &ckksEncoder,
+                                    seal::Evaluator &evaluator) = 0;
 };
 
-class DenseLayer: public Layer {
-  private:
-    Matrix weights;
-    Vector biases;
+class DenseLayer : public Layer {
+ private:
+  Matrix weights;
+  Vector biases;
 
-    // help properties for use by backpropagation algorithm
-    // Matrix nablaW;
-    // Vector nablaB;
-    // out;
-    // out_prime;
+  // help properties for use by backpropagation algorithm
+  // Matrix nablaW;
+  // Vector nablaB;
+  // out;
+  // out_prime;
 
-  public:
-    DenseLayer(Matrix weights, Vector biases);
-    void matmulDiagonal(seal::Ciphertext &in_out, const Matrix &mat, seal::GaloisKeys &galois_keys, seal::CKKSEncoder &ckks_encoder, seal::Evaluator &evaluator);
-    void multiplyCKKSBabystepGiantstep(seal::Ciphertext &in_out, const Matrix &mat, seal::GaloisKeys &galois_keys, seal::CKKSEncoder &ckks_encoder, seal::Evaluator &evaluator);
-    virtual Vector feedforward(Vector x);
-    virtual void feedforwardEncrypted(seal::Ciphertext &in_out, seal::GaloisKeys &galoisKeys, seal::RelinKeys relinKeys, seal::CKKSEncoder &ckksEncoder, seal::Evaluator &evaluator);
+ public:
+  DenseLayer(Matrix weights, Vector biases);
+  void matmulDiagonal(seal::Ciphertext &in_out, const Matrix &mat, seal::GaloisKeys &galois_keys, seal::CKKSEncoder &ckks_encoder,
+                      seal::Evaluator &evaluator);
+  void multiplyCKKSBabystepGiantstep(seal::Ciphertext &in_out, const Matrix &mat, seal::GaloisKeys &galois_keys, seal::CKKSEncoder &ckks_encoder,
+                                     seal::Evaluator &evaluator);
+  virtual Vector feedforward(Vector x);
+  virtual void feedforwardEncrypted(seal::Ciphertext &in_out, seal::GaloisKeys &galoisKeys, seal::RelinKeys relinKeys, seal::CKKSEncoder &ckksEncoder,
+                                    seal::Evaluator &evaluator);
 };
 
-class ActivationLayer: public Layer {
-  public:
-    ActivationLayer() = default;
-    // static Vector activation(Vector x);
-    // static Vector activationPrime(Vector x);
+class ActivationLayer : public Layer {
+ public:
+  ActivationLayer() = default;
+  // static Vector activation(Vector x);
+  // static Vector activationPrime(Vector x);
 
-    virtual Vector feedforward(Vector x);
-    virtual void feedforwardEncrypted(seal::Ciphertext &in_out, seal::GaloisKeys &galoisKeys, seal::RelinKeys relinKeys, seal::CKKSEncoder &ckksEncoder, seal::Evaluator &evaluator);
+  virtual Vector feedforward(Vector x);
+  virtual void feedforwardEncrypted(seal::Ciphertext &in_out, seal::GaloisKeys &galoisKeys, seal::RelinKeys relinKeys, seal::CKKSEncoder &ckksEncoder,
+                                    seal::Evaluator &evaluator);
 };
--- code/classifier/Network.h
+++ code/classifier/Network.h
@@ -2,18 +2,18 @@
 #include "vector"
 
 class Network {
-  private:
-    std::vector<Layer *> layers;
+ private:
+  std::vector<Layer *> layers;
 
-  public:
-    seal::SEALContext *context = nullptr;
+ public:
+  seal::SEALContext *context = nullptr;
 
-  public:
-    Network();
-    void init();
-    void addLayer(Layer *layer);
-    Vector predict(Vector input);
-    seal::Ciphertext predictEncrypted(seal::Ciphertext &ciphertext, seal::RelinKeys &relinKeys, seal::GaloisKeys &galoisKeys);
-    int interpretResult(Vector result);
-    Vector interpretResultProbabilities(Vector result);
+ public:
+  Network();
+  void init();
+  void addLayer(Layer *layer);
+  Vector predict(Vector input);
+  seal::Ciphertext predictEncrypted(seal::Ciphertext &ciphertext, seal::RelinKeys &relinKeys, seal::GaloisKeys &galoisKeys);
+  int interpretResult(Vector result);
+  Vector interpretResultProbabilities(Vector result);
 };
