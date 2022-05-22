6b9878729087c1a3f25a77a4969a77fd66d2fea7
MrP01 <peter@waldert.at>
2022-05-22 17:34:00 +0200

    plot semilogarithmically

--- code/plots/plots.py
+++ code/plots/plots.py
@@ -13,7 +13,7 @@ def plot_rotation_error(ctx, layer1_csv="plots/rotdiff-layer1.csv", layer2_csv="
     fig = plt.figure()
     for i, layer in enumerate((np.loadtxt(layer1_csv), np.loadtxt(layer2_csv)), start=1):
         axes: plt.Axes = fig.add_subplot(2, 1, i)
-        axes.plot(layer, label=r"$\sum (x - x_{true})^2$")
+        axes.semilogy(layer, label=r"$\sum (x - x_{true})^2$")
         # axes.plot(np.sqrt(layer), label=r"$\sqrt{\sum (x - x_{true})^2}$")
         axes.set_xlabel(r"$n$")
         axes.set_ylabel(r"error")
