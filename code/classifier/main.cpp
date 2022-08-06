0bc42b2dd43b2d009fae6070db92688c386597e7
MrP01 <peter@waldert.at>
2022-08-06 12:01:33 +0200

    add a second running classifier instance, nginx accepts localhost as hostname too, log the request start

--- code/classifier/main.cpp
+++ code/classifier/main.cpp
@@ -40,7 +40,7 @@ nlohmann::json handlePlainPredictionRequest(nlohmann::json request) {
 }
 
 nlohmann::json handleEncryptedPredictionRequest(nlohmann::json request) {
-  PLOG(plog::debug) << "Incoming encrypted request";
+  PLOG(plog::info) << "Incoming encrypted request... starting handler";
 
   seal::RelinKeys relinKeys;
   seal::GaloisKeys galoisKeys;
